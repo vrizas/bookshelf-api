21cf845bc3e2c8b8bf40894747569ef20911a9c2
vrizas <vrizasizza@gmail.com>
2022-04-08 07:40:29 +0700

    create delete book api

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,8 @@ const {
   addBookHandler,
   getAllBooksHandler,
   getBookByIdHandler,
-  updateBookHandler
+  updateBookHandler,
+  deleteBookHandler
 } = require('./handler')
 
 const routes = [
@@ -25,6 +26,11 @@ const routes = [
     method: 'PUT',
     path: '/books/{bookId}',
     handler: updateBookHandler
+  },
+  {
+    method: 'DELETE',
+    path: '/books/{bookId}',
+    handler: deleteBookHandler
   }
 ]
 
